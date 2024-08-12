b71c8f5c5efe1f5ae1cc0cb6ea7d2cf0c935133a
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-12 14:50:52 +0300

    addede post request

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios";
+import React, {useEffect, useState} from 'react'
+import axios, {AxiosRequestConfig} from "axios";
 
 export default {
     title: 'API',
 }
 
+const config: AxiosRequestConfig = {
+    withCredentials: true,
+    headers: {'API-KEY': '2b51a744-206a-4e4b-bd70-9b68bff7bc37'}
+}
+
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',{withCredentials: true})
+        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', config)
 
-        promise.then((res)=>{
+        promise.then((res) => {
             setState(res.data)
         })
     }, [])
@@ -19,21 +24,28 @@ export const GetTodolists = () => {
 
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {}, [])
+    useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'MOBX'}, config)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
 
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {}, [])
+    useEffect(() => {
+    }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
 
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {}, [])
+    useEffect(() => {
+    }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
