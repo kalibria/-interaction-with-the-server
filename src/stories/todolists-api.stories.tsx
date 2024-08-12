b1db3915ca5ab9e01f4252898a2c9dd0953211ce
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-12 14:22:09 +0300

    added get request using axios library

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API',
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',{withCredentials: true})
+
+        promise.then((res)=>{
+            setState(res.data)
+        })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {}, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {}, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {}, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
